3a4d20ace5dd85995bcfffeb720a03806036e416
Abdul Wajid <abdulwajidnv@gmail.com>
2022-10-16 12:40:38 +0530

    Page loading animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdviceCard from "./components/advice_card/AdviceCard";
 import "./App.css";
 import Footer from "./components/footer/Footer";
+import Loader from "./components/loader/Loader";
 
 function App() {
-  return (
-    <div className="app">
-      <AdviceCard />
-      <Footer />
-    </div>
-  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="app">
+        <Loader />
+      </div>
+    );
+  } else {
+    return (
+      <div className="app">
+        <AdviceCard />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/advice_card/AdviceCard.jsx
+++ src/components/advice_card/AdviceCard.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AdviceCard.css";
+import axios from "axios";
 
 function AdviceCard() {
+  const [advice, setAdvice] = useState("");
+
+  function getAdvice() {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        setAdvice(res.data.slip.advice);
+      })
+      .catch(() => {
+        setAdvice("Something went wrong. Advice not found !");
+      });
+  }
+  useEffect(() => {
+    getAdvice();
+  });
+
   return (
     <div className="advice-card">
-      <p className="advice">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad sint
-        accusamus alias officia voluptatum.
-      </p>
-      <div className="new__button">Get New Advice</div>
+      <p className="advice">{advice}</p>
+      <div
+        className="new__button"
+        onClick={() => {
+          getAdvice();
+        }}
+      >
+        Get New Advice
+      </div>
     </div>
   );
 }
