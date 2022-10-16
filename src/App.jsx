e365fe587de52760e02368ffebb2e9164837d84f
Abdul Wajid <abdulwajidnv@gmail.com>
2022-10-16 10:54:01 +0530

    Responsive footer and buymeacoffee button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AdviceCard from "./components/advice_card/AdviceCard";
 import "./App.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <AdviceCard />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="attribution">
+        Designed and Developed by{" "}
+        <a href="https://wajid.tech" target="_blank" rel="noopener noreferrer">
+          Wajid
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
