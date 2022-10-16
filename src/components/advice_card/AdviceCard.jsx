086ee54e6c6b78a6b6ebb03c1173ff40d7754a3d
Abdul Wajid <abdulwajidnv@gmail.com>
2022-10-16 13:22:19 +0530

    Dummy loading added to advise card because loading when api fetching not working properly and added red color for not found text

--- src/components/advice_card/AdviceCard.jsx
+++ src/components/advice_card/AdviceCard.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./AdviceCard.css";
 import axios from "axios";
+import Loader from "../loader/Loader";
 
 function AdviceCard() {
   const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
 
   function getAdvice() {
+    setTimeout(() => {
+      setLoading(false);
+    }, 800);
     axios
       .get("https://api.adviceslip.com/advice")
       .then((res) => {
@@ -19,12 +24,20 @@ function AdviceCard() {
     getAdvice();
   });
 
+  let adviceClassName;
+  if (advice === "Something went wrong. Advice not found !") {
+    adviceClassName = "advice advice__not-found";
+  } else {
+    adviceClassName = "advice";
+  }
+
   return (
     <div className="advice-card">
-      <p className="advice">{advice}</p>
+      {loading ? <Loader /> : <p className={adviceClassName}>{advice}</p>}
       <div
         className="new__button"
         onClick={() => {
+          setLoading(true);
           getAdvice();
         }}
       >
